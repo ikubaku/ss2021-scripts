f37b2a250339496686566e703d4e2e9912d503d5
ikubaku <hide4d51@gmail.com>
2021-05-20 01:44:05 +0900

    Fix scripts

--- scripts/extract_pairs_narrowed.sh
+++ scripts/extract_pairs_narrowed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /opt/workdir/Munin/main.py -c /opt/workdir/munin_config.toml -l /mnt/work/munin_find_headers.log -v /mnt/work/result_narrowed_latest /mnt/work/arduino_project /mnt/work/pairs_narrowed_latest
+python3 /opt/workdir/Munin/main.py -c /opt/workdir/munin_config.toml -l /mnt/work/munin_extract_pairs.log -v /mnt/work/result_narrowed_latest /mnt/work/arduino_project /mnt/work/pairs_narrowed_latest
