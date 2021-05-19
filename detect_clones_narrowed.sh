ee48bfff7e1b5633374f34212242eb314db260cb
ikubaku <hide4d51@gmail.com>
2021-05-20 01:43:48 +0900

    Force attach to standard I/O

--- detect_clones_narrowed.sh
+++ detect_clones_narrowed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/create_detect_session_narrowed.sh
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/detect_clones_narrowed.sh
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/extract_pairs_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/create_detect_session_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/detect_clones_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/extract_pairs_narrowed.sh
--- detect_clones_not_narrowed.sh
+++ detect_clones_not_narrowed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/create_detect_session_not_narrowed.sh
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/detect_clones_not_narrowed.sh
-docker run --rm -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/extract_pairs_not_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/create_detect_session_not_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/detect_clones_not_narrowed.sh
+docker run --rm -it -v $PWD/work:/mnt/work ss2021_scripts:latest /opt/workdir/scripts/extract_pairs_not_narrowed.sh
